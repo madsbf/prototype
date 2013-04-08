06680310803503ce3eb15aabc0fed13b9b9f91ca
Du Nguyen <dunguyen90@gmail.com>
2013-04-08 09:34:15 +0200

    Removed error

--- RunForYourMoney/src/dk/partyroulette/runforyourmoney/RunStatisticsActivity.java
+++ RunForYourMoney/src/dk/partyroulette/runforyourmoney/RunStatisticsActivity.java
@@ -4,7 +4,6 @@ import android.app.Activity;
 import android.os.Bundle;
 
 public class RunStatisticsActivity extends Activity{
-	private GoogleMap rMap;
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 	    setContentView(R.layout.activity_run_statistics);
