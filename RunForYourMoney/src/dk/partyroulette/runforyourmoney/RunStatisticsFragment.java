9c120236f169a4b02328d4aba5fb8be955360b55
Du Nguyen <dunguyen90@gmail.com>
2013-04-18 11:25:42 +0200

    Map statistics

--- RunForYourMoney/src/dk/partyroulette/runforyourmoney/RunStatisticsFragment.java
+++ RunForYourMoney/src/dk/partyroulette/runforyourmoney/RunStatisticsFragment.java
@@ -5,12 +5,19 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 public class RunStatisticsFragment extends Fragment 
 {
 	public View onCreateView (LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
 	{
+		super.onCreateView(inflater, container, savedInstanceState);
 		return inflater.inflate(R.layout.fragment_run_statistics, container);
 	}
 
+	public void onStart() {
+		super.onStart();
+		TextView t = (TextView) getView().findViewById(R.id.statsLength);
+		t.setText("5.01" + " KM");
+	}
 }
